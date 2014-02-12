5b993f22b3029f2ade8cee2ca5be5625576d9643
Pengumc <michiel.van.der.coelen@gmail.com>
2014-02-12 15:34:44 +0100

    temp commit

--- src/testdll.c
+++ src/testdll.c
@@ -1,3 +1,46 @@
-int main(){
+#include <stdio.h>
 
+int main(){
+}
+
+//This function is called when 20-sim attaches the dll.
+int Initialize(){
+	FILE* f = fopen("log.txt","a");
+	fprintf(f, "Init\n");
+	fclose(f);
+
+}
+
+//This function is called when 20-sim detaches the dll.
+int Terminate(){
+	FILE* f = fopen("log.txt","a");
+	fprintf(f, "Term\n");
+	fclose(f);
+
+}
+
+//called at the start of a simulation run
+int InitializeRun(){
+	FILE* f = fopen("log.txt","a");
+	fprintf(f, "InitRun\n");
+	fclose(f);
+
+}
+
+//called at the end of a simulation run
+int TerminateRun(){
+	FILE* f = fopen("log.txt","a");
+	fprintf(f, "TermRun\n");
+	fclose(f);
+
+}
+
+int myFunction(double* inarr, int inputs, double* outarr, int outputs, int major){
+	//let's assume input is [time, x]
+	//output is [x+time]
+	FILE* f = fopen("log.txt","a");
+	fprintf(f, "-myFunction called\n");
+	fclose(f);
+	outarr[0] = inarr[1]+inarr[0];
+	return(0);
 }
